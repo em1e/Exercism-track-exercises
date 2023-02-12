b970042d8be6211687214f1425d819de64d37db8
Emi <113087578+em1e@users.noreply.github.com>
2023-02-12 17:41:28 +0200

    Create Transpose.js

---
+++ JavaScrip/Easy/Transpose.js
@@ -0,0 +1,22 @@
+// Given an input text output it transposed.
+// https://en.wikipedia.org/wiki/Transpose
+
+export function transpose(input) {
+
+    if (input.length === 0) {return []}
+    let transposedResult = [];
+    let rows = Math.max(...input.map(elem => elem.length));
+
+    for(let col=0; col<rows; col++){
+        let nString = '';
+        input.forEach((elem) => {
+            elem[col] ? nString += elem[col] : nString += " ";
+        });
+        transposedResult.push(nString);
+    }
+    for (let elem=rows-1; elem>=0; elem--){
+        transposedResult[elem] = transposedResult[elem].trimRight();
+        if (transposedResult[elem].length === input.length) break;
+    }
+    return transposedResult;
+}
