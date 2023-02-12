b858b17744f509bb2f2b278f015f7246d45a1acc
Emi <113087578+em1e@users.noreply.github.com>
2023-02-12 18:21:46 +0200

    Create Difference-of-Squares.py

---
+++ Python/Easy/Difference-of-Squares.py
@@ -0,0 +1,9 @@
+# Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
+def square_of_sum(number):
+    return sum([i for i in range(1, number+1)])**2
+
+def sum_of_squares(number):
+    return sum([i**2 for i in range(1, number+1)])
+
+def difference_of_squares(number):
+    return square_of_sum(number) - sum_of_squares(number)
